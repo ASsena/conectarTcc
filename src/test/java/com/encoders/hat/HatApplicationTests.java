e8bae0c5f37d6f6ab26ad43df62f82337661e5ab
Arthur Sena <139017819+ASsena@users.noreply.github.com>
2025-03-10 15:50:07 -0300

    Add files via upload

---
+++ src/test/java/com/encoders/hat/HatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encoders.hat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
